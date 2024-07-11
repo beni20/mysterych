5131f8811d4dfae00661db89eb7574d9a37f9dfc
y architect <64083403+beni20@users.noreply.github.com>
2024-07-11 22:54:25 +0200

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -11,8 +11,6 @@ import SectionSeparator from "../../components/section-separator";
 import Layout from "../../components/layout";
 import PostTitle from "../../components/post-title";
 import Tags from "../../components/tags";
-import { getAllPostsWithSlug, getPostAndMorePosts } from "../../lib/api";
-import { CMS_NAME } from "../../lib/constants";
 
 export default function Post({ post, posts, preview }) {
   const router = useRouter();
@@ -33,7 +31,7 @@ export default function Post({ post, posts, preview }) {
             <article>
               <Head>
                 <title>
-                  {`${post.title} | Next.js Blog Example with ${CMS_NAME}`}
+                  {`${post.title} | Next.js Blog Example with xx`}
                 </title>
                 <meta
                   property="og:image"
@@ -67,7 +65,7 @@ export const getStaticProps: GetStaticProps = async ({
   preview = false,
   previewData,
 }) => {
-  const data = await getPostAndMorePosts(params?.slug, preview, previewData);
+  const data = {};
 
   return {
     props: {
@@ -80,7 +78,7 @@ export const getStaticProps: GetStaticProps = async ({
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const allPosts = await getAllPostsWithSlug();
+  const allPosts = {};
 
   return {
     paths: allPosts.edges.map(({ node }) => `/posts/${node.slug}`) || [],
